5fe90e5c4c9d1b460fa287118cd5e7acf9fde163
Mahmud Bakale <app.mahmud.bakale@gmail.com>
2023-12-25 08:39:36 +0100

    Wip

--- app/Http/Controllers/Api/SaleController.php
+++ app/Http/Controllers/Api/SaleController.php
@@ -27,7 +27,7 @@ class SaleController extends Controller
             ->where('id', $request->prid)
             ->first();
 
-        $amount = $request->price * $request->qty;
+        $amount = intval($request->price * $request->qty);
 
         $update = DB::table('sales_order')
             ->where('product_id', $request->prid)
@@ -36,7 +36,7 @@ class SaleController extends Controller
             ->update([
                 'quantity' => $request->qty,
                 'amount' => $amount,
-                'price' => $request->price
+                'price' => intval($request->price)
             ]);
         $getAmount = DB::table('sales_order')
             ->where('product_id', $request->prid)
